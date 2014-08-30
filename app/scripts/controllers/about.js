a809811a9dbf1894d9f5a330f76f64a2af725e2e
crullian <crullian@gmail.com>
2014-08-30 16:47:09 -0400

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chordReaderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chordReaderApp
+ */
+angular.module('chordReaderApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chordReaderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chordReaderApp
+ */
+angular.module('chordReaderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
